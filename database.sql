7f11119c6d3981125cf9ed0ca595f2f781aaeb60
colin jaworski <colinrjaworski.84@gmail.com>
2022-02-12 13:22:05 -0600

    added data to database.sql file

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "tasks" (
+	"id" SERIAL PRIMARY KEY,
+	"task" VARCHAR (250) NOT NULL,
+	"dateAdded" DATE,
+  "isComplete" BOOLEAN DEFAULT FALSE
+);
+
+insert into "tasks"
+("task", "dateAdded") 
+VALUES 
+	('finish weekend assignment', '2-12-2022'),
+	('get haircut', '2-12-2022'),
+	('play with dogs', '2-12-2022'),
+	('maybe shower', '2-12-2022')
