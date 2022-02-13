afc7f51ca28fc39910ff1dacfc0c14b4001e7a1d
colin jaworski <colinrjaworski.84@gmail.com>
2022-02-13 17:29:35 -0600

    I am done

--- database.sql
+++ database.sql
@@ -9,10 +9,13 @@ insert into "tasks"
 ("task", "dateAdded") 
 VALUES 
 	('Finish weekend assignment', '2022-02-02'),
+	('Laundry', '2022-02-02'),
+	('shovel driveway', '2022-02-02'),
+	('clean the house', '2022-02-02'),
 	('Get haircut', '2022-02-03'),
 	('Play with dogs', '2022-02-02'),
 	('Maybe shower', '2022-02-02'),
 	('Go grocery shopping', '2022-02-02'),
 	('Call a friend', '2022-01-13'),
-	('Buy a cat', 'Never'),
+	('Buy a cat', 'Nope'),
 
